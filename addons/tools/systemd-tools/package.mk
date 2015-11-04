fa55e2372de7b5f61881b43c92fa964390165f72
Lukas Rusak <lorusak@gmail.com>
2015-11-04 13:50:15 -0800

    import systemd-tools

---
+++ addons/tools/systemd-tools/package.mk
@@ -0,0 +1,62 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2013 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="systemd-tools"
+PKG_VERSION="0"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE=""
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="systemd"
+PKG_PRIORITY="optional"
+PKG_SECTION="system"
+PKG_SHORTDESC="This addon provides extra systemd utilities"
+PKG_LONGDESC="This addon provides extra systemd utilities"
+PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+PKG_ADDON_PROVIDES=""
+PKG_ADDON_REPOVERSION="6.0"
+
+PKG_SECTION="tools"
+PKG_MAINTAINER="Lukas Rusak (lorusak@gmail.com)"
+
+unpack() {
+  :
+}
+
+configure_target() {
+  :
+}
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+  :
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -a $(get_build_dir 'systemd')/.$TARGET_NAME/systemd-nspawn $ADDON_BUILD/$PKG_ADDON_ID/bin/
+}
