ff509e1afc57452fa8453afbda172a72a1df191f
Lukas Rusak <lorusak@gmail.com>
2015-12-05 16:24:58 -0800

    docker: turn into a proper service

--- addons/service/system/docker/source/default.py
+++ addons/service/system/docker/source/default.py
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2013 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
 #
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,4 +16,90 @@
 #  along with OpenELEC.tv; see the file COPYING.  If not, write to
 #  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
 #  http://www.gnu.org/copyleft/gpl.html
-################################################################################ 
+################################################################################
+
+import os
+import subprocess
+import sys
+import time
+import xbmc
+import xbmcaddon
+import xbmcgui
+
+sys.path.append('/storage/.kodi/addons/service.openelec.settings')
+sys.path.append('/usr/share/kodi/addons/service.openelec.settings')
+
+import oe
+
+__author__     = 'lrusak'
+__url__        = 'https://github.com/lrusak/lrusak-openelec-addons'
+__addon__      = xbmcaddon.Addon()
+__path__       = __addon__.getAddonInfo('path')
+__service__    = __path__ + '/systemd/' + __addon__.getAddonInfo('id') + '.service'
+__socket__     = __path__ + '/systemd/' + __addon__.getAddonInfo('id') + '.socket'
+
+class Main(object):
+
+    def __init__(self, *args, **kwargs):
+
+        monitor = DockerMonitor(self)
+
+        if not Docker().is_enabled():
+            Docker().enable()
+            Docker().start()
+
+        while not monitor.abortRequested():
+            if monitor.waitForAbort():
+                Docker().stop()
+                Docker().disable()
+
+class Docker(object):
+
+    def enable(self):
+        self.execute('systemctl enable ' + __service__)
+        self.execute('systemctl enable ' + __socket__)
+
+    def disable(self):
+        self.execute('systemctl disable docker.service')
+        self.execute('systemctl disable docker.socket')
+
+    def is_enabled(self):
+        if self.execute('systemctl is-enabled docker', get_result=1).strip('\n') == 'enabled':
+            return True
+        else:
+            return False
+
+    def start(self):
+        self.execute('systemctl start docker')
+
+    def stop(self):
+        self.execute('systemctl stop docker')
+
+    def is_active(self):
+        if self.execute('systemctl is-active docker', get_result=1).strip('\n') == 'active':
+            return True
+        else:
+            return False
+
+    def execute(self, command_line, get_result=0):
+        result = oe.execute(command_line, get_result=get_result)
+        if get_result:
+            return result
+
+    def restart(self):
+        if self.is_active():
+            self.stop()
+            self.start()
+
+class DockerMonitor(xbmc.Monitor):
+
+    def __init__(self, *args, **kwargs):
+        xbmc.Monitor.__init__(self)
+
+    def onSettingsChanged(self):
+        Docker().restart()
+
+if ( __name__ == "__main__" ):
+    Main()
+
+    del DockerMonitor
