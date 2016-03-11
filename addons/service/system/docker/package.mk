0b0dab0bbcacf507b9bbd4309c8fb9822f93a73a
Lukas Rusak <lorusak@gmail.com>
2016-03-11 21:23:54 +0100

    docker: update to 1.10.3

--- addons/service/system/docker/package.mk
+++ addons/service/system/docker/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="docker"
-PKG_VERSION="1.10.2"
-PKG_REV="3"
+PKG_VERSION="1.10.3"
+PKG_REV="4"
 PKG_ARCH="any"
 PKG_ADDON_PROJECTS="Generic RPi RPi2"
 PKG_LICENSE="ASL"
